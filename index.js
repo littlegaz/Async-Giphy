810a37d890efb1577d1e78a60fc959c71b213bcc
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-13 17:22:34 +0100

    it works

--- index.js
+++ index.js
@@ -13,31 +13,16 @@ require('dotenv').config();
 //     console.log( "Gif Url:", gifUrl );
 //     return gifUrl;}
 
-async function getImage(query, random = false) {
-    try {
-      const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+async function getImage(query) {
+
+      const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
       const response = await fetch(endpoint);
       const data = await response.json();
-  
-      let gifUrl;
-      if (random) {
-        const randomIndex = Math.floor(Math.random() * 25);
-        gifUrl = data.data[randomIndex].images.original.url;
-      } else {
-        const gifUrls = data.data.map((gif) => gif.images.original.url);
-        return gifUrls;
-      }
-  
-      console.log(gifUrl);
-      return gifUrl;
-    } catch (error) {
-      console.error('Error fetching gif:', error);
-      throw error;
-    }
-  }
-  
-  getImage('cats', true);
-  getImage('cats');
+      const gifUrl = data.data[0].url
+      console.log("Gif Url:", gifUrl)
+      return gifUrl
+}
+  getImage(`cats`);
 
     // return data
 
@@ -48,7 +33,7 @@ async function getImage(query, random = false) {
     // catch (error){
     //     console.error("Failed to get image", error)
     // }
-}
+// }
 
 
 // .then(data => console.log(data));
