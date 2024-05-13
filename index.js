e6143cf2a34194250045aa5bd632518b80044560
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-13 15:54:59 +0100

    clueless

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 require('dotenv').config();
 
 // const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=XzgW9znTnJorYyKnPIB7XHsLUTeQUSZz&q=llama&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
-const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+// const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
 
 
 async function getImage(query){
     try{
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=XzgW9znTnJorYyKnPIB7XHsLUTeQUSZz&q=${query}&limit=25&offset=0&rating=g&lang=en`
     const response = await fetch(endpoint);
     const data = await response.json();
-    return data
+    // return data
+
+    let gifLink = data.url[0]
+    return gifLink
+
 }
     catch (error){
         console.error("Failed to get image", error)
     }
+}
 
-console.log(getImage(llama))
+getImage('llama').then(data => console.log(data));
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
