f718aafe8d389c65dc9056d021d642529b94cbec
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-13 14:44:36 +0100

    i have the api key and link

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
 
+const endpoint = https://api.giphy.com/v1/gifs/search?api_key=XzgW9znTnJorYyKnPIB7XHsLUTeQUSZz&q=llama&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
+
+
+
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
